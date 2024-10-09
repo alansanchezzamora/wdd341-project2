c3188d860b1727a9851dc144e9094252277a3f5a
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-10-08 22:43:31 -0600

    3000 port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongodb = require("./data/database");
 const bodyParser = require("body-parser");
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 //routes work accross sites
