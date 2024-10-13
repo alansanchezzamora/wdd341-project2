953638b622f9cda2324da3fc2bad35395aa772bd
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-10-12 21:44:32 -0600

    fix?

--- server.js
+++ server.js
@@ -64,24 +64,17 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-app.get("/", (req, res) => {
-  res.send(
-    req.session.user !== undefined
-      ? `Logged in as ${req.session.user.displayName}`
-      : "Logged Out"
-  );
+// OAuth endpoints
+app.get('/', (req, res) => {
+  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out");
 });
 
-app.get(
-  "/github/callback",
-  passport.authenticate("github", {
-    failureRedirect: "/api-docs",
-    session: false,
-  }),
-  (req, res) => {
-    req.session.user = req.user;
-    res.redirect("/");
-  }
+// GitHub callback
+app.get('/github/callback', passport.authenticate('github', { failureRedirect: '/api-docs', session: false }),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    }
 );
 
 mongodb.initDb((err) => {
