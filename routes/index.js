c4339b03153f4eeb640ce1d256058d908b217daf
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-10-16 22:13:59 -0600

    fix?

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,6 @@ router.use("/income", require("./income"));
 
 
 router.get("/login", passport.authenticate("github"), (req, res) => {});
-router.get("/logout", function (req, res, next) {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-
-
-
-
-
-
-
-router.get("/login", passport.authenticate("github"), (req, res) => {});
 
 router.get("/logout", function (req, res, next) {
   req.logout(function (err) {
